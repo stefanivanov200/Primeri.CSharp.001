938c13b866353b4216df81b5c0d336862478f799
стефаниванов <kapi18@mail.bg>
2016-01-13 19:54:14 +0200

    създаване на програма Хелло Аула

---
+++ Hello.Aula/Hello.Aula/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello.Aula
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.ReadKey ();
+		}
+	}
+}
